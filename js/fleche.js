638511b04938ddaa192532b7ef317d5903e7ac85
Carabistouille-fr <leomagnien07@gmail.com>
2026-02-28 19:46:47 +0100

    first commit

---
+++ js/fleche.js
@@ -0,0 +1,16 @@
+<<<<<<< HEAD
+const fleche = document.querySelector(".fleche_descendre");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+        fleche.classList.add("fleche_disparait");
+    }
+=======
+const fleche = document.querySelector(".fleche_descendre");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+        fleche.classList.add("fleche_disparait");
+    }
+>>>>>>> ebe4cb9 (fix img directories)
+});
