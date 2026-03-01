076bf2fa03d267c1a5c74e083f08d88964a00126
Carabistouille-fr <leomagnien07@gmail.com>
2026-03-01 01:19:53 +0100

    fix some bugs

--- js/fleche.js
+++
@@ -1,7 +0,0 @@
-const fleche = document.querySelector(".fleche_descendre");
-
-window.addEventListener("scroll", () => {
-    if (window.scrollY > 50) {
-        fleche.classList.add("fleche_disparait");
-    }
-});
