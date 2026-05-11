ffbbd67d4958391b54c3a9145d6d8fff7b7a0295
Carabistouille-fr <leomagnien07@gmail.com>
2026-05-11 14:52:32 +0200

    it seems so perfect to me

--- js/bulle_conversation.js
+++ js/bulle_conversation.js
@@ -1,4 +1,4 @@
-const PROFILE_PHOTO = "./assets/pp/pp_head.jpg";
+const PROFILE_PHOTO = "./assets/pp/pp.png";
 const PROFILE_INITIAL = "L";
 const PROFILE_NAME = "Léo · Dev & Designer";
 const PROFILE_STATUS = "Disponible pour de nouveaux projets";
